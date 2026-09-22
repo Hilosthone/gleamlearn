87c06d42254e77c45e59cf81c01d65ea5d893980
Soliu Sulyman <solihullahsulyman@gmail.com>
2026-09-22 09:45:50 +0100

    feat: xml file added to improve google search

---
+++ src/app/sitemap.ts
@@ -0,0 +1,27 @@
+// src/app/sitemap.ts
+import { MetadataRoute } from 'next';
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const baseUrl = 'https://gleamlearn.vercel.app';
+
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 1.0,
+    },
+    {
+      url: `${baseUrl}/auth/login`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.5,
+    },
+    {
+      url: `${baseUrl}/auth/signup`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.5,
+    },
+  ];
+}
